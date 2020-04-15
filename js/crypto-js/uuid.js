0b5b59c363b613900f7851f6bd186d369b107e73
nyz123 <1083206106@qq.com>
2020-04-15 10:53:04 +0800

    random string

--- js/crypto-js/uuid.js
+++ js/crypto-js/uuid.js
@@ -21,6 +21,16 @@ export function getBDUuid(e) {
         return r
 }
 
+export function randomStr(n = 32) {
+  let $chars =
+    "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678"; /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+  let maxPos = $chars.length;
+  let pwd = "";
+  for (let i = 0; i < n; i++) {
+    pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return pwd;
+}
 
 export function copyValue(value) {
   if (document.execCommand("copy")) {
